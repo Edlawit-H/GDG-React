0c594f2c4eba54882b7ee13cb0e57680479e7fc5
Edlawit-H <edlawithuluwork82@gmail.com>
2025-03-20 21:09:46 +0300

    Update and rename Week5-UserAuthApp/AuthStatus.js to Week5-UserAuthApp/pages/Home.jsx

---
+++ Week5-UserAuthApp/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>Click on Profile to log in or log out.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
