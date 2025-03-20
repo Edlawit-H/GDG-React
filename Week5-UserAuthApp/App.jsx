17dc0691104ba8c1406f31cda9c5f38114c21d24
Edlawit-H <edlawithuluwork82@gmail.com>
2025-03-20 21:02:38 +0300

    Update and rename App.js to App.jsx

---
+++ Week5-UserAuthApp/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
