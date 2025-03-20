1e07bd7eb4ac6ffe45f49978b386310d0881f127
Edlawit-H <edlawithuluwork82@gmail.com>
2025-03-20 20:38:04 +0300

    Update and rename UserProfile.js to UserProfile.jsx

--- Week4-ReactNavigation/pages/UserProfile.js
+++
@@ -1,8 +0,0 @@
-// src/pages/UserProfile.js
-import React from "react";
-
-function UserProfile() {
-  return <h2>User Profile Page</h2>;
-}
-
-export default UserProfile;
---
+++ Week4-ReactNavigation/pages/UserProfile.jsx
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+function UserProfile() {
+  const { username } = useParams();
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>Welcome, {username}!</p>
+    </div>
+  );
+}
+
+export default UserProfile;
